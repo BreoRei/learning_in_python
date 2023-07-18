6191f94ae19b4ed59561ca43346c87d8e9c28e8e
BreoRei <artemii0307@gmail.com>
2023-07-18 22:19:39 +0500

    adding a constant

--- homework_1/task_1.py
+++ homework_1/task_1.py
@@ -22,9 +22,9 @@ def convert_to_numbers(str_list: list[str]) -> list[float]:
 
 def what_is_this_triangle(num_list: list[float]) -> None:
     if triangle_whether(num_list):
-        if num_list[0] == num_list[1] and num_list[0] == num_list[2]:
+        if num_list[0] == num_list[1] == num_list[2]:
             print("Это равносторонний треугольник!")
-        elif num_list[0] == num_list[1] or num_list[1] == num_list[2] or num_list[0] == num_list[2]:
+        elif num_list[0] == num_list[1] or num_list[0] == num_list[2] or num_list[1] == num_list[2]:
             print("Это равнобедренный треугольник!")
         else:
             print("Это разносторонний треугольник!")
@@ -34,22 +34,23 @@ def what_is_this_triangle(num_list: list[float]) -> None:
 
 def triangle_whether(num_list: list[float]) -> bool:
     checkbox: bool = True
-    if num_list[0] > num_list[1] + num_list[2]:
+    if num_list[0] >= num_list[1] + num_list[2]:
         checkbox = False
-    elif num_list[1] > num_list[0] + num_list[2]:
+    elif num_list[1] >= num_list[0] + num_list[2]:
         checkbox = False
-    elif num_list[2] > num_list[0] + num_list[1]:
+    elif num_list[2] >= num_list[0] + num_list[1]:
         checkbox = False
     return checkbox
 
 
 @decorator("Начало работы.", "Завершение работы.")
 def get_side_values():
+    NUMBER_OF_PARTIES = 3
     try:
         data: list[str] = input("Введите три стороны треугольника через пробел --> ").split()
-        if len(data) > 3:
+        if len(data) > NUMBER_OF_PARTIES:
             print("Вы ввели больше трёх значений!")
-        elif len(data) < 3:
+        elif len(data) < NUMBER_OF_PARTIES:
             print("Вы ввели меньше трёх значений!")
         else:
             triangle_sides: list[float] = convert_to_numbers(str_list=data)
